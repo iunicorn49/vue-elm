88302abb011b88f4eeb7fb6d4ca86135dca4b26e
zlk <734775779@qq.com>
2018-03-22 11:45:41 +0800

    header 完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,30 +17,87 @@
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
-			<div class="support-count" v-if="seller.supports">
+			<div @click="showDetail" class="support-count" v-if="seller.supports">
 				<span class="count">{{seller.supports.length}}个</span>
 				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
-		<div class="bulletin-wrapper">
+		<!-- 公告栏 -->
+		<div class="bulletin-wrapper" @click="showDetail">
 			<!-- 消除空格引起的间隙 -->
 			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
 			<i class="icon-keyboard_arrow_right"></i>
 		</div>
+		<!-- 头部背景图片 -->
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%">
 		</div>
+		<!-- 全屏弹窗 -->
+		<transition name="fade">
+			<div class="detail" v-show="detailShow">
+				<div class="detail-wrapper clearfix">
+					<div class="detail-main">
+						<h1 class="name">{{seller.name}}</h1>
+						<div class="star-wrapper">
+							<star :size="48" :score="seller.score"></star>
+						</div>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">优惠信息</div>
+							<div class="line"></div>
+						</div>
+						<ul v-if="seller.supports" class="supports">
+							<li class="support-item"
+								v-for="(item, index) in seller.supports"
+							:key="index">
+								<span class="icon" :class="classMap[seller.supports[index].type]"></span>
+								<span class="text">{{seller.supports[index].description}}</span>
+							</li>
+						</ul>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">商家公告</div>
+							<div class="line"></div>
+						</div>
+						<div class="bulletin">
+							<p class="content">{{seller.bulletin}}</p>
+						</div>
+					</div>
+				</div>
+				<div class="detail-close" @click.stop="hideDetail">
+					<i class="icon-close"></i>
+				</div>
+			</div>
+		</transition>
 	</div>
 </template>
 
 <script>
+	import Star from '../star/star'
+
 	export default {
 		name: 'v-header',
 		props: {
 			seller: { type: Object, default: {} }
 		},
+		data() {
+			return {
+				detailShow: false
+			}
+		},
 		created() {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+		},
+		methods: {
+			showDetail() {
+				this.detailShow = true
+			},
+			hideDetail() {
+				this.detailShow = false
+			}
+		},
+		components: {
+			Star
 		}
 	}
 </script>
@@ -52,6 +109,7 @@
 		color #fff
 		background rgba(7,17,27,.5)
 		position relative
+		overflow hidden
 		.content-wrapper
 			position relative
 			padding 24px 12px 18px 24px
@@ -158,4 +216,92 @@
 			height 100%
 			z-index -1
 			filter: blur(10px)
+		.detail
+			position fixed
+			top 0
+			left 0 
+			z-index 100
+			width 100%
+			height 100%
+			overflow auto
+			.detail-wrapper
+				min-height 100%
+				width 100%
+				background rgba(7,17,27,.8)
+				.detail-main
+					margin-top 64px
+					padding-bottom 64px
+					.name
+						line-height 16px
+						text-align center
+						font-size 16px
+						font-weight 700
+					.star-wrapper
+						margin-top 18px
+						padding 2px 0
+						text-align center
+					.title
+						display flex
+						width 80%
+						margin 28px auto 24px auto
+						.line
+							flex: 1
+							position relative
+							top -6px
+							border-bottom 1px solid rgba(255,255,255,.2)
+						.text
+							padding 0 12px
+							font-size 14px
+							font-weight 700
+					.supports
+						width: 80%
+						margin 0 auto
+						.support-item
+							padding 0 12px
+							margin-bottom 12px
+							font-size 0
+							&:last-child
+								margin-bottom 0
+							.icon
+								display inline-block
+								width 16px
+								height 16px
+								margin-right 6px
+								vertical-align top
+								background-size 16px 16px
+								background-repeat no-repeat
+								&.decrease
+									bg-image('decrease_2')
+								&.discount
+									bg-image('discount_2')
+								&.guarantee
+									bg-image('guarantee_2')
+								&.invoice
+									bg-image('invoice_2')
+								&.special
+									bg-image('special_2')
+							.text
+								font-size 12px
+								line-height 16px
+					.bulletin
+						width: 80%
+						margin 0 auto
+						.content
+							padding 0 12px
+							line-height 24px
+							font-size 12px
+			.detail-close
+				position relative
+				width 32px
+				height 32px
+				margin -64px auto 0 auto
+				clear both
+				font-size 32px
+
+
+	.fade-enter-active, .fade-leave-active
+		transition: all .5s
+		opacity 1
+	.fade-enter, .fade-leave-active
+		opacity 0
 </style>
