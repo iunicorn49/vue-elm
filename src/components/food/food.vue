4f582b0070cb576f2ad756e6a0f655047ac9a085
zlk <734775779@qq.com>
2018-03-28 17:33:25 +0800

    debug;

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,16 +1,48 @@
 <template>
 	<transition name="slide">
-		<div class="food" v-show="showFlag">
+		<div ref="food" class="food" v-show="showFlag">
 			<div class="food-content">
 				<div class="image-header">
-					<img :src="food">
-				</div>	
-			</div>		
+					<img :src="food.image">
+					<div class="back" @click.stop="hide">
+						<i class="icon-arrow_lift"></i>
+					</div>
+				</div>
+			</div>	
+			<div class="content">
+				<h1 class="title">{{food.name}}</h1>
+				<div class="detail">
+					<span class="sell-count">月售{{food.sellCount}}份</span>
+					<span class="rating">好评率{{food.rating}}</span>
+				</div>
+				<div class="price">
+					<span class="now">${{food.price}}</span><span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
+				</div>
+				<div class="cartcontrol-wrapper">
+					<cart-control :food="food"></cart-control>
+				</div>
+				<div @click.stop="addFirst" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>	
+			</div>
+			<split v-show="food.info"></split>
+			<div class="info" v-show="food.info">
+				<h1 class="title">商品信息</h1>
+				<p class="text">{{food.info}}</p>
+			</div>
+			<split></split>
+			<div class="rating">
+				<h1 class="title">商品评价</h1>
+				<rating-select></rating-select>
+			</div>
 		</div>
 	</transition>
 </template>
 
 <script>
+	import BScroll from 'better-scroll'
+	import CartControl from 'components/cartControl/cartControl'
+	import Split from '../split/split'
+	import RatingSelect from '../ratingselect/ratingselect'
+
 	export default {
 		props: {
 			food: {type: Object, default() {return {}}}
@@ -20,13 +52,31 @@
 				showFlag: false
 			}
 		},
+
 		methods: {
+			addFirst() {
+				this.$set(this.food, 'count', 1)
+			},
 			show() {
 				this.showFlag = true
+				this.$nextTick(() => {
+					if (!this.scroll) {
+						this.scroll = new BScroll(this.$refs.food, {
+							click: true
+						})
+					} else {
+						this.scroll.refresh()
+					}
+				})
 			},
 			hide() {
 				this.showFlag = false
 			}
+		},
+		components: {
+			CartControl,
+			Split,
+			RatingSelect
 		}
 	}
 </script>
@@ -44,5 +94,85 @@
 		&.slide-enter, &.slide-leave-active
 			transform translate3d(100%,0,0)
 		&.slide-enter-active, &.slide-leave-active
-			transition all .5s linear
+			transition all .2s linear
+		.image-header
+			position relative
+			width 100%
+			height 0
+			padding-top 100%
+			img
+				position absolute
+				top 0
+				left 0
+				width 100%
+				height 100%
+			.back
+				position absolute
+				top 10px
+				left 0
+				.icon-arrow_lift
+					display block 
+					padding 10px
+					font-size 20px
+					color #fff
+		.content
+			padding 18px
+			position relative
+			.title
+				line-height 14px
+				margin-bottom 8px
+				font-size 14px
+				font-weight 700
+				color rgb(7,17,27)
+			.detail
+				margin-bottom 18px
+				line-height 10px
+				height 10px
+				font-size 0
+				.sell-count, .rating
+					font-size 10px
+					color rgb(147,153,159)
+				.sell-count
+					margin-right 12px
+			.price
+				font-weight 700
+				line-height 24px
+				.now
+					margin-right 8px
+					font-size 14px
+					color rgb(240,20,20)
+				.old
+					text-decoration line-through
+					font-size 10px
+					color rgb(147,153,159)
+			.cartcontrol-wrapper
+				position absolute
+				right 12px
+				bottom 12px
+			.buy
+				position absolute
+				right 18px
+				bottom 18px
+				z-index 10
+				height 24px
+				line-height 24px
+				padding 0 12px
+				box-sizing border-box
+				font-size 10px
+				border-radius 12px
+				color #fff
+				background rgb(0,160,220)
+
+		.info
+			padding 18px
+			.title
+				line-height 17px
+				margin-bottom 6px
+				font-size 14px
+				color rgb(7,17,27)
+			.text
+				padding 0 8px
+				font-size 12px
+				line-height 24px
+				color rgb(77,85,93)
 </style>
