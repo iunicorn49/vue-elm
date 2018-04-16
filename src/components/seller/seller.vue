0248b4fcefb6e73b4e90245b4c703ba9b2a1d7e1
zlk <734775779@qq.com>
2018-04-16 18:59:29 +0800

    合体

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -42,7 +42,18 @@
 						<span class="icon" :class="classMap[seller.supports[index].type]"></span>
 						<span class="text">{{seller.supports[index].description}}</span>
 					</li>
-				</ul>
+				</ul>	
+			</div>
+			<split></split>
+			<div class="pics">
+				<h1 class="title">商家实景</h1>
+				<div ref="picWrapper" class="pic-wrapper">
+					<ul ref="picList" class="pic-list">
+						<li class="pic-item" v-for="(pic,index) in seller.pics" :key="index">
+							<img width="120" height="90" :src="pic">
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -61,13 +72,56 @@
 		created() {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
 		},
-		mounted() {
-			this.$nextTick(() => {
-				this.scroll = new BScroll(this.$refs.seller, {
-					click: true
-				})
-			})
-		},
+    watch: {
+      'seller'() {
+        this.$nextTick(() => {
+          this._initScroll();
+          this._initPics();
+        });
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this._initScroll();
+        this._initPics();
+      });
+    },
+    methods: {
+      toggleFavorite(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+        saveToLocal(this.seller.id, 'favorite', this.favorite);
+      },
+      _initScroll() {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.seller, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      },
+      _initPics() {
+        if (this.seller.pics) {
+          let picWidth = 120;
+          let margin = 6;
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          this.$refs.picList.style.width = width + 'px';
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              });
+            } else {
+              this.picScroll.refresh();
+            }
+          });
+        }
+      }
+    },
 		components: {
 			BScroll,Star,Split
 		}
@@ -144,6 +198,8 @@
 					padding 16px 12px
 					font-size 0
 					border-1px(rgba(7,17,27,.1))
+					&:last-child
+						border none
 				.icon
 					display inline-block
 					vertical-align top
@@ -166,4 +222,25 @@
 					font-size 12px
 					line-height 16px
 					color rgb(7,17,27)
+
+		.pics
+			padding 18px
+			.title
+				margin-bottom 12px
+				font-size 14px
+				color rgb(7,17,27)
+				font-size 14px
+			.pic-wrapper
+				width 100%
+				overflow hidden
+				white-space nowrap
+				.pic-list
+					font-size 0
+					.pic-item
+						display inline-block
+						margin-right 6px
+						width 120px
+						height 90px
+						&:last-child
+							margin-right 0
 </style>
